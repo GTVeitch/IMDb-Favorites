9d4e02053d669843c1f1a827ef3ac3aab18c47da
Andrew McLean <andrew.mclean.5895@gmail.com>
2022-10-03 12:05:23 -0600

    Basic Skeleton API Angry

--- newFile.js
+++ newFile.js
@@ -1,22 +1,19 @@
-// const originalDeck = []; 
-// const dummyArray = [34, 3, 3,3, 43, 3,3, 4,4]
-// console.log(dummyArray)
-// fetch('https://www.deckofcardsapi.com/api/deck/new/draw/?count=52')
-//     .then(response => response.json())
-//     .then(data => /*originalDeck.push(data)*/{for(let counter = 0; counter < 52; counter++){
-//         originalDeck.push(data.cards[counter])
-//     }})
-//     //.then(originalDeck.code[0])
-//     .then(console.log(originalDeck))
-//     .then(console.log(originalDeck.cards))
-    //switch  per game statement 
-        //War, Solitaire, 
+const myFavorites = []; 
+const myLeastFavorites = [];
+let movieList = [];
 
-    //logic
-        //storage arrays: player 1/2 hands, player 1/2 decks, player 1/2 field, player 1/2 discard pile, inPlay
-        // Function inputs: for the functions will be:  # of cards, which player, (taken from and given too)) 
-        // Functions://deckToHand(), handToDeck(), handToField(), fieldToHand(), fieldToDiscard(), discardToField(), shuffleCards(), 
-        // discardToDeck(), deckToDiscard(), handToDiscard(),  discardToHand()
+fetch(`https://imdb-api.com/en/API/Top250Movies/k_9m771wic`)
+    .then(response => response.json())
+    .then(data => movieList.push(data))
+    .then(console.log(movieList))
 
-        // function sourceToDestination(cardNum, fromPlayer, toPlayer) {
-            //for(const counter  = 0;  0 < cardNum; 0++){destinationToPlayer.push(sourceFromPlayer())} }
+    
+
+// let navBar = document.querySelector('#list')  
+// function newFunction() {
+//     navBar.addEventListener('click', (e) => {
+//     e.preventDefault()
+//     console.log('you mad it')
+// })}
+
+// newFunction()
